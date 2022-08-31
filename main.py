0d39106a0722305b96053c143ecf24147f507c91
Ulysses Lopes <ulysses.rlopes@gmail.com>
2022-08-31 15:52:48 -0300

    Add function for send email with attachments

--- main.py
+++ main.py
@@ -1,19 +1,63 @@
 import smtplib
 import email.message
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 # configure email message
 
 # first define your body text to send
-body = """<h1> Hello, Ulysses </h1>
-<p>  Test send email with python </p>
+body = """<h1> Olá, Ulysses </h1>
+<p>  Teste de Envio de Email com python</p>
 """
 
 subject = 'Test Send Email with Python Function'
-my_from = ' your email '
-my_to = ' email to '
-my_password = '###############'
+my_from = ' here your email from '
+my_to = ' here your email to '
+my_password = '##########'
+host = 'smtp.gmail.com'
+port = '587'
 
 
+# function for send emails with attachments
+def send_email_with_files():
+    try:
+        server = smtplib.SMTP(host,port)
+        server.ehlo()
+        server.starttls()
+        server.login(my_from, my_password)
+
+        email_msg = MIMEMultipart()
+        email_msg['From'] = my_from
+        email_msg['To'] = my_to
+        email_msg['Subject'] = subject
+        email_msg.attach(MIMEText(body, 'html'))
+
+        cam_arquivo = './files/sendtest.pdf'
+        attachment = open(cam_arquivo, 'rb')
+
+        att = MIMEBase('application', 'octet-stream')
+        att.set_payload(attachment.read())
+        encoders.encode_base64(att)
+
+        att.add_header('Content-Disposition', f'attachment; filename=sendtest.pdf')
+        attachment.close()
+
+        email_msg.attach(att)
+
+        server.sendmail(email_msg['From'],email_msg['To'],email_msg.as_string())
+        server.quit()
+
+        msg = 'Email and File send!'
+        print(msg)
+
+    except:
+        msg = 'Email and File not send!'
+        print(msg)
+
+
+# function for send a normal email without files
 def send_email():
     body_email = body
 
@@ -27,13 +71,24 @@ def send_email():
         msg.add_header('Content-Type', 'text/html')
         msg.set_payload(body_email)
 
-        s = smtplib.SMTP('smtp.gmail.com: 587')
+        s = smtplib.SMTP(host,port)
         s.starttls()
 
         s.login(msg['From'], password)
         s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
 
-        print('Email Enviado')
+        msg = 'Email Send!'
+        print(msg)
 
     except:
-        print('Email não enviado')
+        msg = 'Email not Send!'
+        print(msg)
+
+
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 from main import send_email
+from main import send_email_with_files
 
-send_email()
+# send_email()
+
+send_email_with_files()
+
+
+
