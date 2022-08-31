6f878fa10c9bd5ddfa4fd5b502281c32b4379008
Ulysses Lopes <ulysses.rlopes@gmail.com>
2022-08-31 11:40:41 -0300

    Add exceptions and new variables for config body email

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import smtplib
 import email.message
 
+# configure email message
+
+# first define your body text to send
+body = """<h1> Olá, Ulysses </h1>
+<p>  Teste de Envio de Email </p>
+"""
+
+subject = 'Test Send Email with Python Function'
+my_from = 'ulysses.rlopes@gmail.com'
+my_to = 'ulysses.rlopes@gmail.com'
+my_password = 'mlqtlyittczuanhb'
+
 
 def send_email():
-    body_email = """
-    <p> Hello, </p>
-    <p> This function send email with python. </p>
-    """
+    body_email = body
 
     msg = email.message.Message()
-    msg['Subject'] = "Teste de Envio de Email com Python"
-    msg['From'] = "your email"
-    msg['To'] = " email to "
-    password = " your pass "
-    msg.add_header('Content-Type', 'text/html')
-    msg.set_payload(body_email)
+    msg['Subject'] = subject
+    msg['From'] = my_from
+    msg['To'] = my_to
+    password = my_password
 
-    s = smtplib.SMTP('smtp.gmail.com: 587')
-    s.starttls()
+    try:
+        msg.add_header('Content-Type', 'text/html')
+        msg.set_payload(body_email)
 
-    s.login(msg['From'], password)
-    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-    print('Email Enviado')
+        s = smtplib.SMTP('smtp.gmail.com: 587')
+        s.starttls()
+
+        s.login(msg['From'], password)
+        s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+
+        print('Email Enviado')
+
+    except:
+        print('Email não enviado')
